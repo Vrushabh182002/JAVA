f0f311426417a131d0263d66eb06cccb7dffd0bd
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-08-12 13:04:31 +0530

    Add files via upload

---
+++ Ceilingofanumber.java
@@ -0,0 +1,26 @@
+public class Ceilingofanumber {
+    public static void main(String[] args) {
+        int a[]={2,3,5,8,9,10,14,19,20,29};
+        int target=16;
+        System.out.println(ceiling(a,target));
+    }
+    static int ceiling(int a[],int target){
+        int s=0,e=a.length-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(a.length==0){
+                return -1;
+            }
+            if(target==a[m]){
+                return a[m];
+            }
+            else if(target>a[m]){
+                s=m+1;
+            }
+            else{
+                e=m-1;
+            }
+        }
+        return a[s];
+    }
+}
