2456a09e3c3761972ec7922669c81e083a696c89
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-08-25 12:20:58 +0530

    Add files via upload

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int a[]={10,2,4,-1,0,3};
+        insertion(a);
+        System.out.println(Arrays.toString(a));
+    }
+    static void insertion(int a[]){
+        for (int i = 0; i < a.length-1; i++){
+            for (int j = i+1; j >0 ; j--){
+                if(a[j]<a[j-1]){
+                    int temp =a[j];
+                    a[j]=a[j-1];
+                    a[j-1]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+}
