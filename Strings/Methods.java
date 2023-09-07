097240162c821e1c124a72ffc8f8131898cfaf7e
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-09-07 10:31:04 +0530

    Add files via upload

---
+++ Strings/Methods.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Methods {
+    public static void main(String[] args) {
+        String names = "Vrushabh Somani";
+        System.out.println(Arrays.toString( names.toCharArray()));
+        System.out.println(names.toLowerCase());
+        System.out.println(names.toUpperCase());
+        System.out.println(names);
+        //and many more methods are there..
+    }
+}
