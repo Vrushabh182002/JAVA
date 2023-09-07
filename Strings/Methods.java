4a3b4270c15efabc8efa89a226b2dfbbc9a68f9a
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-09-07 10:39:45 +0530

    Add files via upload

--- Strings/Methods.java
+++ Strings/Methods.java
@@ -1,11 +1,13 @@
 import java.util.Arrays;
 public class Methods {
     public static void main(String[] args) {
-        String names = "Vrushabh Somani";
-        System.out.println(Arrays.toString( names.toCharArray()));
+        String names = "Vrushabh Somani Hello  ";
         System.out.println(names.toLowerCase());
         System.out.println(names.toUpperCase());
-        System.out.println(names);
+        System.out.println(Arrays.toString(names.split(" ")));
+        System.out.println(Arrays.toString( names.toCharArray()));
+        System.out.println("   VRUSHABH SOMANI   ".strip());
+        System.out.println();
         //and many more methods are there..
     }
 }
