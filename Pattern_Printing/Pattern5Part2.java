d5e47bf914bb94e0704ddf105aeee841f39e8506
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-09-09 15:32:07 +0530

    Add files via upload

---
+++ Pattern_Printing/Pattern5Part2.java
@@ -0,0 +1,17 @@
+public class Pattern5Part2 {
+    public static void main(String[] args) {
+        int i,j;
+        for (i = 1; i<=5; i++) {
+            for (j = 1; j<=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (i = 4; i>=0; i--) {
+            for (j = 1; j<=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
