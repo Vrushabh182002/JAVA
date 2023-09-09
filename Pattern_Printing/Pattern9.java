6fbd1c3adca01be2f89f786fb1aa799c49b08177
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-09-09 15:49:57 +0530

    Add files via upload

---
+++ Pattern_Printing/Pattern9.java
@@ -0,0 +1,17 @@
+public class Pattern9 { 
+    public static void main(String[] args) {
+        int i,j,s,n=5;
+        for(i=1;i<=n;i++){
+            for(s=0;s<n-i;s++){
+                System.out.print(" ");
+            }
+            for(j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
