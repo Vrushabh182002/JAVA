c7acda455c6b4a89ec066596d686f4afdd3e6c0b
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-11-04 15:09:35 +0530

    Add files via upload

---
+++ Recursion/ReverseNumber.java
@@ -0,0 +1,16 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 12345;
+        rev(num);
+        System.out.println(sum);
+    }
+    static int sum=0;
+    static void rev(int n){
+        if(n==0){
+            return;
+        }
+        int rem=n%10;
+        sum=sum*10+rem;
+        rev(n/10);
+    }
+}
