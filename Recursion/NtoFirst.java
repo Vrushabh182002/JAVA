4eb1ddd98b4897dbd937d815ebe6beca87ea91b3
VRUSHABH SOMANI <110593917+Vrushabh182002@users.noreply.github.com>
2023-11-04 14:21:41 +0530

    Add files via upload

---
+++ Recursion/NtoFirst.java
@@ -0,0 +1,13 @@
+public class NtoFirst {
+    public static void main(String[] args) {
+        int n=5;
+        fun(n);
+    }
+    static void fun(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n+" ");
+        fun(n-1);
+    }
+}
