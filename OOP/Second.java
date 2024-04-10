66e3df085ef5d76ebcffed98db92211699706d3f
VRUSHABH SOMANI <vssomani2002@gmail.com>
2024-04-10 17:28:54 +0530

    pass

---
+++ OOP/Second.java
@@ -0,0 +1,9 @@
+public class Second {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 25.36;
+       
+        System.out.println("Integer: " + a); 
+        System.out.println("Double: " + b);     
+    }
+}
